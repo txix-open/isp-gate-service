1a46b678efb9d8472e6eda0274b8b4193a0fa1af
DeniskaRediska <grj.den@gmail.com>
2019-11-07 18:08:17 +0300

    up approve matcher

--- approve/matcher/runtime_matcher.go
+++ approve/matcher/runtime_matcher.go
@@ -11,7 +11,13 @@ type runtimeMatcher struct {
 func (mm runtimeMatcher) Match(method string) []string {
 	resp := make([]string, 0)
 	for _, pattern := range mm.patterns {
-		if strings.HasPrefix(method, pattern) {
+		patternLen := len(pattern)
+		if patternLen > 0 && pattern[patternLen-1] == '*' {
+			pattern = pattern[:patternLen-1]
+			if strings.HasPrefix(method, pattern) {
+				resp = append(resp, pattern)
+			}
+		} else if strings.EqualFold(method, pattern) {
 			resp = append(resp, pattern)
 		}
 	}
