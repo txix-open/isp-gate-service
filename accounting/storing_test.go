3c801a4c099526fe9caf6773991539cee46c3254
DeniskaRediska <grj.den@gmail.com>
2019-11-21 13:06:18 +0300

    fix storing test

--- accounting/storing_test.go
+++ accounting/storing_test.go
@@ -10,41 +10,105 @@ import (
 	"time"
 )
 
-var unloadingSetting = conf.StoringSetting{
-	Size:    3,
-	Timeout: "50ms",
-}
+var (
+	timeoutSetting = conf.StoringSetting{
+		Size:    3,
+		Timeout: "30ms",
+	}
 
-func TestStoringTask(t *testing.T) {
-	a := assert.New(t)
-	repository := &requestsRepository{cache: make([]entity.Request, 0)}
+	bufferSetting = conf.StoringSetting{
+		Size:    3,
+		Timeout: "2s",
+	}
+
+	unloadSetting = conf.StoringSetting{
+		Size:    3,
+		Timeout: "100ms",
+	}
+)
+
+func initStoring(setting conf.StoringSetting) (*requestsRepository, error) {
+	repository := &requestsRepository{cache: make([]entity.Request, 0), wg: &sync.WaitGroup{}}
 	model.RequestsRep = repository
 
-	err := storage.Init(unloadingSetting)
+	err := storage.Init(setting)
+	return repository, err
+}
+
+func wait(wg *sync.WaitGroup) bool {
+	done := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+	select {
+	case <-done:
+		return true
+	case <-time.After(time.Second):
+		return false
+	}
+}
+
+func TestStoringTask_Stop(t *testing.T) {
+	a := assert.New(t)
+	rep, err := initStoring(bufferSetting)
 	a.NoError(err)
 
-	for i, j := range []string{"1", "2", "3", "4", "5"} {
-		storage.TakeRequest(int32(i), j, time.Now())
-	}
-	a.Len(repository.cache, 3)
-	time.Sleep(time.Millisecond * 60)
-	a.Len(repository.cache, 5)
-
+	rep.wg.Add(2)
 	for i, j := range []string{"1", "2", "3", "4", "5"} {
 		storage.TakeRequest(int32(i), j, time.Now())
 	}
 	storage.Stop()
-	a.Len(repository.cache, 10)
+	a.Len(rep.cache, 5)
+}
+
+func TestStoringTask_Buffer(t *testing.T) {
+	a := assert.New(t)
+	rep, err := initStoring(bufferSetting)
+	a.NoError(err)
+
+	rep.wg.Add(1)
+	for i, j := range []string{"1", "2", "3", "4", "5"} {
+		storage.TakeRequest(int32(i), j, time.Now())
+	}
+	a.True(wait(rep.wg))
+	a.Len(rep.cache, 3)
+}
+
+func TestStoringTask_Timeout(t *testing.T) {
+	a := assert.New(t)
+	rep, err := initStoring(timeoutSetting)
+	a.NoError(err)
+
+	rep.wg.Add(1)
+	for i, j := range []string{"1", "2"} {
+		storage.TakeRequest(int32(i), j, time.Now())
+	}
+	a.True(wait(rep.wg))
+	a.Len(rep.cache, 2)
+}
+
+func TestStoringTask_Unload(t *testing.T) {
+	a := assert.New(t)
+	rep, err := initStoring(unloadSetting)
+	a.NoError(err)
+
+	rep.wg.Add(2)
+	for i, j := range []string{"1", "2", "3", "4", "5"} {
+		storage.TakeRequest(int32(i), j, time.Now())
+	}
+	a.True(wait(rep.wg))
+	a.Len(rep.cache, 5)
 }
 
 type requestsRepository struct {
 	cache []entity.Request
 	mx    sync.Mutex
+	wg    *sync.WaitGroup
 }
 
 func (r *requestsRepository) Insert(model []entity.Request) error {
-	r.mx.Lock()
 	r.cache = append(r.cache, model...)
-	r.mx.Unlock()
+	r.wg.Done()
 	return nil
 }
