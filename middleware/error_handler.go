f9f127f6b9deb1c7d887446dfea7afd0068baff7
d1slike <merser1q@gmail.com>
2022-07-26 21:38:01 +0300

    MSP-437 implement error handler

---
+++ middleware/error_handler.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/integration-system/isp-kit/log"
+	"isp-gate-service/httperrors"
+	"isp-gate-service/request"
+)
+
+type HttpError interface {
+	WriteError(w http.ResponseWriter) error
+}
+
+func ErrorHandler(logger log.Logger) Middleware {
+	return func(next Handler) Handler {
+		return HandlerFunc(func(ctx *request.Context) error {
+			err := next.Handle(ctx)
+			if err == nil {
+				return nil
+			}
+
+			logger.Error(ctx.Context(), err)
+
+			httpErr, ok := err.(HttpError)
+			if ok {
+				return httpErr.WriteError(ctx.ResponseWriter())
+			}
+
+			return httperrors.
+				New(http.StatusInternalServerError, "internal service error", err).
+				WriteError(ctx.ResponseWriter())
+		})
+	}
+}
