bd4b52e5cc044e8e8215ab0799f3e35ac9103106
d1slike <merser1q@gmail.com>
2022-07-26 21:24:33 +0300

    MSP-437 implement simple cache

---
+++ cache/cache_test.go
@@ -0,0 +1,47 @@
+package cache_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"isp-gate-service/cache"
+)
+
+func TestGetBasic(t *testing.T) {
+	require := require.New(t)
+
+	cache := cache.New()
+	cache.Set("key", []byte("data"), 24*time.Hour)
+
+	data, ok := cache.Get("key")
+	require.True(ok)
+	require.EqualValues("data", data)
+
+	_, ok = cache.Get("key2")
+	require.False(ok)
+}
+
+func TestGetExpired(t *testing.T) {
+	require := require.New(t)
+
+	cache := cache.New()
+	cache.Set("key", []byte("data"), 500*time.Millisecond)
+
+	time.Sleep(1 * time.Second)
+
+	data, ok := cache.Get("key")
+	require.False(ok)
+	require.Nil(data)
+}
+
+func TestNilValue(t *testing.T) {
+	require := require.New(t)
+
+	cache := cache.New()
+	cache.Set("key", nil, 24*time.Hour)
+
+	data, ok := cache.Get("key")
+	require.True(ok)
+	require.Nil(data)
+}
